2b6a4263a73bf202abb8fe522a1a25856824ef1f
Stefany Newman <stef@stefanywebdesign.info>
2022-10-07 08:55:04 -0500

    added states

--- src/index.js
+++ src/index.js
@@ -11,8 +11,9 @@ class Square extends React.Component {
   }
     render() {
       return (
-        <button className="square" onClick={function() {console.log('clicked')}}>
-           {this.props.value}
+        <button className="square" 
+        onClick={() => this.setState({value: 'X'})}>
+           {this.state.value}
         </button>
       );
     }
